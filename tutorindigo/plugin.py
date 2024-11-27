83e91d94992c773d244ac7f2e2591b1de9c0010a
Amir Tadrisi <amirtds@gmail.com>
2024-11-27 16:40:42 -0500

    chore: update brand and avatar condition

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -101,7 +101,7 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "mfe-dockerfile-post-npm-install-learning",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#a1f67a02fe07a220a6cc548bca1e850e078d0050'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#77b05da74ac4b65c512f6c8c348137b76ff23c09'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.1.3'
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 
@@ -119,7 +119,7 @@ RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.1.1'
         (
             "mfe-dockerfile-post-npm-install-discussions",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#a1f67a02fe07a220a6cc548bca1e850e078d0050'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#77b05da74ac4b65c512f6c8c348137b76ff23c09'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.1.3'
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 
@@ -129,7 +129,7 @@ COPY indigo/env.config.jsx /openedx/app/
         (
             "mfe-dockerfile-post-npm-install-learner-dashboard",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#a1f67a02fe07a220a6cc548bca1e850e078d0050'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#77b05da74ac4b65c512f6c8c348137b76ff23c09'
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 
 COPY indigo/env.config.jsx /openedx/app/
@@ -138,7 +138,7 @@ COPY indigo/env.config.jsx /openedx/app/
         (
             "mfe-dockerfile-post-npm-install-profile",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#a1f67a02fe07a220a6cc548bca1e850e078d0050'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#77b05da74ac4b65c512f6c8c348137b76ff23c09'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.1.3'
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 
@@ -148,7 +148,7 @@ COPY indigo/env.config.jsx /openedx/app/
         (
             "mfe-dockerfile-post-npm-install-account",
             """
-RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#a1f67a02fe07a220a6cc548bca1e850e078d0050'
+RUN npm install '@edx/brand@git+https://github.com/amirtds/brand-openedx.git#77b05da74ac4b65c512f6c8c348137b76ff23c09'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.1.3'
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 
